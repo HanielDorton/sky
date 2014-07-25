895a5780ddd7429c9225e88372755c6a43e250c3
root <root@haniel-HP-Pavilion-g6-Notebook-PC.(none)>
2014-07-25 12:01:25 -0700

    Initial Commit

---
+++ desktop/src/com/haniel/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.haniel.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.haniel.game.Sky;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Sky(), config);
+	}
+}
